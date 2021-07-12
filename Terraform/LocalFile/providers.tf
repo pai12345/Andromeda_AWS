e39005121c29fe1e0178dfcb42fd7c90b43235ea
Rahul Pai <rahulpai123sap@gmail.com>
2021-07-12 20:33:08 +0530

    Added Remote State - S3 to local file

--- Terraform/LocalFile/providers.tf
+++ Terraform/LocalFile/providers.tf
@@ -5,4 +5,11 @@ terraform {
       version = "3.49.0"
     }
   }
+  backend "s3" {
+    bucket = "andrterraformbackend"
+    encrypt = true
+    key    = "prod/terraform.tfstate"
+    # dynamodb_table = "Table_name"
+    region = "us-east-1"
+  }
 }
--- Terraform/S3/Bucket/modules/BucketPolicy/main.tf
+++ Terraform/S3/Bucket/modules/BucketPolicy/main.tf
@@ -1,5 +1,45 @@
 resource "aws_s3_bucket_policy" "andr_s3bucketpolicy" {
   bucket = var.bucket
+  # policy = jsonencode({
+  #    "Version": "2012-10-17",
+  #    "Id": "PutObjPolicy",
+  #    "Statement": [
+  #          {
+  #               "Sid": "DenyIncorrectEncryptionHeader",
+  #               "Effect": "Deny",
+  #               "Principal": "*",
+  #               "Action": "s3:PutObject",
+  #               "Resource": "${var.bucketarn}/*",
+  #               "Condition": {
+  #                       "StringNotEquals": {
+  #                              "s3:x-amz-server-side-encryption": "AES256"
+  #                        }
+  #               }
+  #          },
+  #          {
+  #               "Sid": "DenyUnEncryptedObjectUploads",
+  #               "Effect": "Deny",
+  #               "Principal": "*",
+  #               "Action": "s3:PutObject",
+  #               "Resource": "${var.bucketarn}/*",
+  #               "Condition": {
+  #                       "Null": {
+  #                              "s3:x-amz-server-side-encryption": true
+  #                       }
+  #              }
+  #          },
+  #           {
+  #             "Sid": "Stmt1626076822782",
+  #             "Action": [
+  #               "s3:GetObject"
+  #             ],
+  #             "Effect": "Allow",
+  #             "Resource": "${var.bucketarn}/*",
+  #             "Principal": "*"
+  #           }
+  #    ]
+  # })
+
   policy = jsonencode({
      "Version": "2012-10-17",
      "Id": "PutObjPolicy",
@@ -31,11 +71,16 @@ resource "aws_s3_bucket_policy" "andr_s3bucketpolicy" {
             {
               "Sid": "Stmt1626076822782",
               "Action": [
-                "s3:GetObject"
+                "s3:GetObject","s3:ListBucket"
               ],
               "Effect": "Allow",
               "Resource": "${var.bucketarn}/*",
               "Principal": "*"
+            },
+            {
+              "Effect": "Allow",
+              "Action": ["s3:GetObject", "s3:PutObject"],
+              "Resource": "${var.bucketarn}/prod/terraform.tfstate"
             }
      ]
   })
