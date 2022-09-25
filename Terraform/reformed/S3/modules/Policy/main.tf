cebf26bb7cec4de5ca1ac55785e8942ee25a57e4
pai12345 <rahulpai123sap@gmail.com>
2022-09-25 21:27:35 +0200

    S3 added mandatory TLS to policy

--- Terraform/reformed/S3/modules/Policy/main.tf
+++ Terraform/reformed/S3/modules/Policy/main.tf
@@ -2,41 +2,54 @@
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = var.bucketid
   policy = jsonencode({
-	"Id": "Policy1652430342619",
-	"Version": "2012-10-17",
-	"Statement": [{
-			"Sid": "Stmt1652430340613",
-			"Action": [
-				"s3:GetObject"
-			],
-			"Effect": "Allow",
-			"Resource": "${var.bucketarn}/*",
-			"Principal": "*"
-		},
-		{
-			"Sid": "DenyIncorrectEncryptionHeader",
-			"Effect": "Deny",
-			"Principal": "*",
-			"Action": "s3:PutObject",
-			"Resource": "${var.bucketarn}/*",
-			"Condition": {
-				"StringNotEquals": {
-					"s3:x-amz-server-side-encryption": "AES256"
-				}
-			}
-		},
-		{
-			"Sid": "DenyUnEncryptedObjectUploads",
-			"Effect": "Deny",
-			"Principal": "*",
-			"Action": "s3:PutObject",
-			"Resource": "${var.bucketarn}/*",
-			"Condition": {
-				"Null": {
-					"s3:x-amz-server-side-encryption": true
-				}
-			}
-		}
-	]
-})
+    "Id" : "Policy1652430342619",
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowTLSRequestsOnly",
+        "Effect" : "Deny",
+        "Principal" : "*",
+        "Action" : "s3:*",
+        "Resource" : "${var.bucketarn}/*",
+        "Condition" : {
+          "Bool" : {
+            "aws:SecureTransport" : "false"
+          }
+        }
+      },
+      {
+        "Sid" : "Stmt1652430340613",
+        "Action" : [
+          "s3:GetObject"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "${var.bucketarn}/*",
+        "Principal" : "*"
+      },
+      {
+        "Sid" : "DenyIncorrectEncryptionHeader",
+        "Effect" : "Deny",
+        "Principal" : "*",
+        "Action" : "s3:PutObject",
+        "Resource" : "${var.bucketarn}/*",
+        "Condition" : {
+          "StringNotEquals" : {
+            "s3:x-amz-server-side-encryption" : "AES256"
+          }
+        }
+      },
+      {
+        "Sid" : "DenyUnEncryptedObjectUploads",
+        "Effect" : "Deny",
+        "Principal" : "*",
+        "Action" : "s3:PutObject",
+        "Resource" : "${var.bucketarn}/*",
+        "Condition" : {
+          "Null" : {
+            "s3:x-amz-server-side-encryption" : true
+          }
+        }
+      }
+    ]
+  })
 }
